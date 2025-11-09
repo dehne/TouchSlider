09c639f531330d25437b4b194aa79c69e90d6946
dehne <dehne1@comcast.net>
2025-11-09 15:03:26 -0800

    General tidying up of the example

--- examples/BasicUsage/src/BasicUsage.cpp
+++ examples/BasicUsage/src/BasicUsage.cpp
@@ -1,57 +1,42 @@
 /****
- * @file    main.cpp
+ * @file    BasicUsage.cpp
  * @author  D. L. Ehnebuske (dle.com@ehnebuske.net)
  * @brief   Experiment to see if I can implement a capacitive touch sensor in a really simple way.
- * @version 0.1
- * @date    2025-09-09
+ * @version 1.0.0
+ * @date    2025-11-09
  * 
  ****
- * Copyright (C) 2024-2025 D. L. Ehnebuske
+ * Copyright (C) 2025 D. L. Ehnebuske
  * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+ * documentation files (the "Software"), to deal in the Software without restriction, including without
+ * limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the 
+ * Software, and to permit persons to whom the Software is furnished to do so, subject to the following 
+ * conditions:
  * 
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions 
+ * of the Software.
  * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE. 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
+ * TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ * DEALINGS IN THE SOFTWARE. 
  * 
  ****/
 #include <Arduino.h>
 #include <TouchSensor.h>
 #include <TouchSlider.h>
 
-#define BANNER          F("\nCapTouch 0.2")
-#define INIT_MILLIS     (2000)                            // millis() to delay at startup
-#define SENSOR_COUNT    (4)                               // The number of sensors we have
-#define SENSOR_A_PIN    (2)                               // GPIO to which sensor "A" is attached
-#define SENSOR_B_PIN    (3)                               // GPIO to which sensor "B" is attached
-#define SENSOR_C_PIN    (4)                               // GPIO to which sensor "C" is attached
-#define SENSOR_D_PIN    (5)                               // GPIO to which sensor "D" is attached
-
-constexpr int32_t SLIDER_MIN = -100;                      // The lowest the slider can be
-constexpr int32_t SLIDER_MAX = 100;                       // The highest the slider can be
-
-#define DEBUG                                             // Uncomment to enable debugging code
-
-/**
- * @brief Hacky convenience function to convert a sensor index into its name.
- * 
- * @return char 
- */
-inline char sensorName(uint8_t i) {
-  return (char)('A' + i);  
-}
+#define BANNER          F("\nTouchSlider Basic Example V1.0.0")
+constexpr unsigned long INIT_MILLIS =   2000;             // millis() to delay at startup
+constexpr uint8_t       SENSOR_COUNT =  4;                // The number of sensors we have
+constexpr uint8_t       SENSOR_A_PIN =  2;                // GPIO to which sensor "A" is attached
+constexpr uint8_t       SENSOR_B_PIN =  3;                // GPIO to which sensor "B" is attached
+constexpr uint8_t       SENSOR_C_PIN =  4;                // GPIO to which sensor "C" is attached
+constexpr uint8_t       SENSOR_D_PIN =  5;                // GPIO to which sensor "D" is attached
+constexpr int32_t       SLIDER_MIN =    -100;             // The lowest the slider can be
+constexpr int32_t       SLIDER_MAX =    100;              // The highest the slider can be
 
 uint8_t pins[] = {SENSOR_A_PIN, SENSOR_B_PIN, SENSOR_C_PIN, SENSOR_D_PIN};
 TouchSlider slider {pins, SENSOR_COUNT};
